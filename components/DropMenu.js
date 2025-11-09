299f64c0f1a3be56364c50556fb07b168929b8a9
Lakshay3766 <lakshaymadaan376@gmail.com>
2025-11-10 02:39:28 +0530

    Again

--- components/DropMenu.js
+++ components/DropMenu.js
@@ -232,24 +232,7 @@ export default function DropMenu() {
                 </Link>
               )}
             </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/guestbook">
-                  <a
-                    className={classNames(
-                      active
-                        ? 'bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300'
-                        : 'bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    <div className="flex flex-row">
-                      <ChatBubbleIcon className="mr-4 mt-0.5" /> Guestbook
-                    </div>
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
+            // Guestbook removed
             <Menu.Item>
               {({ active }) => (
                 <Link href="/uses">
